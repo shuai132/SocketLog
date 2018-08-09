ef57eef96c0920af6e92cb4fbf00178204a33902
liushuai <liushuai@xiaoyezi.com>
2018-08-09 18:04:06 +0800

    refactor: refine code

--- socketlog/SocketLog.h
+++ socketlog/SocketLog.h
@@ -12,67 +12,9 @@
 #include <queue>
 #include <condition_variable>
 #include "lib/tcpacceptor.h"
+#include "msg.h"
 
 class SocketLog {
-    struct Msg {
-        typedef unsigned char byte;
-
-        size_t len = 0;
-        byte* data = nullptr;
-
-        inline void initMsg(const void* data = nullptr, size_t len = 0) {
-            if (data == nullptr || len == 0) {
-                this->len  = 0;
-                this->data = nullptr;
-                return;
-            }
-
-            this->len  = len;
-            this->data = new byte[len];
-            memcpy(this->data, data, len);
-        }
-
-        inline void moveMsg(Msg& msg) {
-            this->len   = msg.len;
-            this->data  = msg.data;
-            msg.data = nullptr;
-        }
-
-        explicit Msg(const void* data = nullptr, size_t len = 0) {
-            initMsg(data, len);
-        }
-
-        Msg(const Msg& msg) {
-            initMsg(msg.data, msg.len);
-        }
-
-        Msg(Msg&& msg) noexcept {
-            moveMsg(msg);
-        }
-
-        Msg& operator=(const Msg& msg) {
-            if (&msg == this)
-                return *this;
-
-            delete[] this->data;
-            initMsg(msg.data, msg.len);
-            return *this;
-        }
-
-        Msg& operator=(Msg&& msg) noexcept {
-            if (&msg == this)
-                return *this;
-
-            delete[] this->data;
-            moveMsg(msg);
-            return *this;
-        }
-
-        ~Msg() {
-            delete[] data;
-        }
-    };
-
 public:
     static SocketLog* getInstance();
 
---
+++ socketlog/msg.h
@@ -0,0 +1,70 @@
+//
+// Created by liushuai on 2018/8/9.
+//
+
+#ifndef MSG_H
+#define MSG_H
+
+#include <cstddef>
+#include <cstring>
+
+struct Msg {
+    typedef unsigned char byte;
+
+    size_t len = 0;
+    byte* data = nullptr;
+
+    inline void initMsg(const void* data = nullptr, size_t len = 0) {
+        if (data == nullptr || len == 0) {
+            this->len  = 0;
+            this->data = nullptr;
+            return;
+        }
+
+        this->len  = len;
+        this->data = new byte[len];
+        memcpy(this->data, data, len);
+    }
+
+    inline void moveMsg(Msg& msg) {
+        this->len   = msg.len;
+        this->data  = msg.data;
+        msg.data = nullptr;
+    }
+
+    explicit Msg(const void* data = nullptr, size_t len = 0) {
+        initMsg(data, len);
+    }
+
+    Msg(const Msg& msg) {
+        initMsg(msg.data, msg.len);
+    }
+
+    Msg(Msg&& msg) noexcept {
+        moveMsg(msg);
+    }
+
+    Msg& operator=(const Msg& msg) {
+        if (&msg == this)
+            return *this;
+
+        delete[] this->data;
+        initMsg(msg.data, msg.len);
+        return *this;
+    }
+
+    Msg& operator=(Msg&& msg) noexcept {
+        if (&msg == this)
+            return *this;
+
+        delete[] this->data;
+        moveMsg(msg);
+        return *this;
+    }
+
+    ~Msg() {
+        delete[] data;
+    }
+};
+
+#endif //MSG_H
